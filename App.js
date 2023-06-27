931b0f16a03a594457ac4aa44a9e6095411cb8cd
amanmadhukar63 <amanmadhukar63@gmail.com>
2023-06-27 12:13:17 +0530

    first commit ever

---
+++ App.js
@@ -0,0 +1,17 @@
+const root=ReactDOM.createRoot(document.getElementById("root"));
+const h=React.createElement("h1",{id:"h"},"I'm React");
+
+
+/* 
+<div id="heading">
+<h1></h1>
+<p></p>
+</div> 
+*/
+const h1=React.createElement("h1",{id:"h1"},"heading1");
+const p=React.createElement("p",{id:"p"},"paragraph");
+const head=React.createElement("div",{id:"heading"},[h1,p]);
+
+// const tags=React.createElement("div",{},[h,head]);
+root.render(head);
+// root.render(head);
