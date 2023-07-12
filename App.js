db5751b5b478945c69b6bc5799f4ce3726825235
amanmadhukar63 <amanmadhukar63@gmail.com>
2023-07-12 16:18:42 +0530

    ignited the app-third commit

--- App.js
+++ App.js
@@ -13,8 +13,28 @@ const h=React.createElement("h1",{id:"h"},"I'm React");
 */
 const h1=React.createElement("h1",{id:"h1"},"heading1");
 const p=React.createElement("p",{id:"p"},"paragraph");
-const head=React.createElement("div",{id:"heading"},[h1,p]);
+const head1=React.createElement("div",{id:"heading"},[h1,p]);
+
+// jsx elment
+const head= <h1>hi i am jsx</h1>;
+
+// functional component
+
+const Component= ()=> (
+    <div>
+        {head}
+        <h1>hello</h1>
+        <h1>Hi i am component</h1>
+    </div>
+);
+const Component2= ()=> (
+    <div>
+        <h1>hello</h1>
+        <h1>Hi i am component</h1>
+        <Component />
+    </div>
+);
 
 // const tags=React.createElement("div",{},[h,head]);
-root.render(head);
+root.render(<Component2 />);
 // root.render(head);
